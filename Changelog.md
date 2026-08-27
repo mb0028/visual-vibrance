f8ffceefc421d3b4c0b2243633d75af6a9a4c8b3
mb28 <244608971+mb0028@users.noreply.github.com>
2026-08-27 07:09:22 +0330

    Version 0.3.6-mb28-fork

---
+++ Changelog.md
@@ -0,0 +1,10 @@
+## v0.3.6-mb28-fork
+
+- Tested on Minecraft v26.2
+- Remove vanilla clouds option because of bugs
+- Add profile selection
+- Change default SHADOW_SOFTNESS value to 0.0
+- Change default shadowMapResolution value to 128
+- Change default SATURATION value to 1.1
+- Change default sunPathRotation value to 25.0
+<!-- - Add 2 more options for shadowMapResolution: 96x & 64x -->
---
+++ Packager/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System.IO.Compression;
+var path = "E:\\visual-vibrance\\Visual Vibrance.zip";
+
+if (Path.Exists(path))
+    File.Delete(path);
+
+var f = new FileStream(path, FileMode.CreateNew);
+ZipFile.CreateFromDirectory("E:\\visual-vibrance\\shaders", f, CompressionLevel.Fastest, true);
+f.Dispose();
+
+var z = await ZipFile.OpenAsync(path, ZipArchiveMode.Update);
+await z.CreateEntryFromFileAsync("E:\\visual-vibrance\\LICENSE", "LICENSE");
+z.Dispose();
